a7250814dc8cd33197792acd34d54737835c661a
tiagocomti <tiago.alexandre.oliveira@hotmail.com>
2021-03-25 19:20:44 -0300

    Subindo

--- Oracle_Api/Controllers/WeatherForecastController.cs
+++ Oracle_Api/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Oracle_Api.Controllers
 {
@@ -17,15 +18,43 @@ namespace Oracle_Api.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
-
+        public Countries paises;
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public Countries Get()
         {
+
+            using (OracleConnection connection = new OracleConnection("User Id=hr;Password=oracle;Data Source=//192.168.0.24:1521/orcl"))
+            {
+                using (OracleCommand cmd = connection.CreateCommand())
+                {
+                    try
+                    {
+                        connection.Open();
+                        cmd.BindByName = true;
+                        cmd.CommandText = "select * from countries";
+                        OracleDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read()) {
+                            this.paises = new Countries();
+                            this.paises.Country_name = reader.GetString(1);
+                            this.paises.Country_ID = reader.GetString(0);
+                            this.paises.Region_id = Convert.ToInt32(reader.GetString(2));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                connection.Close();
+            }
+            return this.paises;
+            /**
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -34,6 +63,7 @@ namespace Oracle_Api.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+            */
         }
     }
 }
---
+++ Oracle_Api/Countries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oracle_Api
+{
+    public class Countries
+    {
+        public string Country_ID { get; set; }
+
+        public string Country_name { get; set; }
+
+        public int Region_id { get; set; }
+
+    }
+}
